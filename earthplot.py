13c7f7cd8425554d33289f1f34e488ca9ce8be7b
nie-yb <60766289+nie-yb@users.noreply.github.com>
2025-01-20 09:30:46 -0800

    2025.01.20

--- earthplot.py
+++ earthplot.py
@@ -1,5 +1,4 @@
 # %%
-
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
@@ -300,7 +299,7 @@ def geoticks(ax, labels=False, labelsize='large',
 def xyticks(ax, xticks=None, yticks=None, xticklabels=None, yticklabels=None,
             xlim=None, ylim=None, zlim=None, sharex=False, sharey=False,
             xlabel=None, ylabel=None, labelsize='large', # xrotation=0, yrotation=0,
-            xminor=None, yminor=None, xtick_params=None, ytick_params=None):
+            xminor=None, yminor=None, xtick_params=None, ytick_params=None, **kwargs):
 
     if xlim is not None:
         ax.set_xlim(*xlim)
@@ -322,8 +321,8 @@ def xyticks(ax, xticks=None, yticks=None, xticklabels=None, yticklabels=None,
         ytick_params = {}
 
     xticks_kw, yticks_kw = {}, {}
-    xticks_dict = {'xticks':'ticks', 'xticklabels':'labels'}
-    yticks_dict = {'yticks':'ticks', 'yticklabels':'labels'}
+    xticks_dict = {'xticks':'ticks', 'xticklabels':'labels', 'xha':'ha'}  # ha doesn't work
+    yticks_dict = {'yticks':'ticks', 'yticklabels':'labels', 'yha':'ha'}
 
     # 遍历函数的所有参数
     for key, value in locals().items():
@@ -666,3 +665,118 @@ def generate_multiples(base_num, start, end):
     return all_multiples[loc]
 
 
+def heatmap(ax, data, xlabels, ylabels, vmin=None, vmax=None, gridcolor='k', gridlinewidth=1, 
+            spines_visible=True, valfmt="{x:.1f}", text_threshold=None, textkw={},
+            **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current Axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    # Plot the heatmap
+    im = ax.imshow(data, vmin=vmin, vmax=vmax, **kwargs)
+
+    # Create colorbar
+    # cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    # cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]), labels=xlabels)  # , ha="right"
+    ax.set_yticks(np.arange(data.shape[0]), labels=ylabels)
+
+    # Let the horizontal axes labeling appear on top.
+    # ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+    # ax.tick_params(axis='x', rotation=-30)
+
+    # Rotate the tick labels and set their alignment.
+    # plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+            #  rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(spines_visible)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color=gridcolor, linestyle='-', linewidth=gridlinewidth)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    _ = annotate_heatmap(im, valfmt=valfmt, threshold=text_threshold, **textkw)
+
+    return im
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+        # print(f'threshold = {threshold}')
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[im.norm(abs(data[i, j])) > threshold])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
